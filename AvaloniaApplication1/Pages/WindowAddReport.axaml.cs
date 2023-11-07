7869ec23353d3e6763237af8965de852d95ad3c7
TSystem <2329192@mail.ru>
2023-11-07 13:21:54 +0700

    Кнопка AddReport

--- AvaloniaApplication1/Pages/WindowAddReport.axaml.cs
+++ AvaloniaApplication1/Pages/WindowAddReport.axaml.cs
@@ -1,33 +1,47 @@
-﻿using Avalonia;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using AvaloniaApplication1.Model;
+using AvaloniaApplication3.Model;
 
 namespace AvaloniaApplication1.Pages;
 
 public partial class WindowAddReport : Window
 {
     private Execution execution;
+    private List<RepairRequest> RepairRequestsList { get; set; }
     public WindowAddReport(Execution execution)
     {
        
         InitializeComponent();
         this.execution = execution;
-        //TODO: Добавить данные на фому
+        UpdateComboBox();
+        CBoxRequest.SelectedItem = RepairRequestsList.Where(w => w.ID == execution.ID).First() as RepairRequest;
+        CBoxRequest.IsEnabled = false;
     }
-
+    private void UpdateComboBox()
+    {
+        RepairRequestsList = DataBaseManager.GetCRepairRequests();
+        
+        CBoxRequest.Items.Clear();
+        
+        CBoxRequest.ItemsSource = RepairRequestsList; ;
+    }
     private void BtnAdd_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
-            //if (TBoxName.Text.Length <= 0 || TBoxPhoneNumber.Text.Length <= 0)
-      //  {
-       //     MessageBoxManager.GetMessageBoxStandard("Ошибка", "Данные не добавлены", ButtonEnum.Ok).ShowAsync();
-      //      return;
-       // }
-        
-        
-      //  DataBaseManager.AddClients(new Client(0,TBoxName.Text, TBoxPhoneNumber.Text));
+        DataBaseManager.AddReports(new Report(
+            0, 
+            Convert.ToInt32(NUpDownTimeSpent.Value), 
+            Convert.ToInt32(TBoxCost.Text), 
+            TBoxFailureReason.Text,
+            TBoxAssisment.Text,
+            ((RepairRequest)CBoxRequest.SelectedItem).ID
+        ));
+        this.Close();
     }
 
     private void BtnBack_OnClick(object? sender, RoutedEventArgs e)
